79ba8e12fd091d02a904e320c646d057fc8151be
rubabramzan96 <rubabramzan96@gmail.com>
2018-01-10 10:00:28 +0000

    Add project files.

---
+++ examRepeat1617/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(examRepeat1617.Startup))]
+namespace examRepeat1617
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
